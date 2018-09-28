a1178158344c4f689edfc1d7d587bb5a0a14421d
Alejandro Carnero <carneroalejandro@gmail.com>
2018-09-28 19:41:48 -0300

    Init SemanticUi

--- pwa-app/src/index.js
+++ pwa-app/src/index.js
@@ -6,7 +6,7 @@ import configureStore from './store';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import '../semantic/dist/semantic.min.css';
 
 ReactDOM.render(
     <Provider store={configureStore()}>
