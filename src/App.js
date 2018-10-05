bd3493dec5323626a728a5de693e5d1e6617f132
Alejandro Carnero <carneroalejandro@gmail.com>
2018-10-05 19:05:31 -0300

    Add react router and pwa base settings

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.jpg';
 import './App.css';
+import { Route } from 'react-router';
+import { Link, BrowserRouter } from 'react-router-dom';
+
+
+const Page = ({ title }) => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>{title}</h2>
+    </div>
+    <p className="App-intro">
+      This is the {title} page.
+    </p>
+    <p>
+      <Link to="/home">Home</Link>
+    </p>
+    <p>
+      <Link to="/about">About</Link>
+    </p>
+    <p>
+      <Link to="/settings">Settings</Link>
+    </p>
+  </div>
+);
+const Home = (props) => (
+  <Page title="Home" />
+);
+const About = (props) => (
+  <Page title="About" />
+);
+const Settings = (props) => (
+  <Page title="Settings" />
+);
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/settings" component={Settings} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
